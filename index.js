7da76524e952fbce13329cf103f7708de247bc81
mustaqimbd <mustaqimkhanbd@gmail.com>
2023-05-03 08:49:54 +0600

    Chefs all data API cteated done

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const cors = require('cors');
-app.use(cors);
+app.use(cors());
+
+const chefsData = require('./Data/chef.json');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send(chefsData)
 })
 
 app.listen(port, () => {
